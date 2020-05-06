240a700761f31f93d91faae6db6e4fb3411c8e9d
jsong073 <jaehasong073@gmail.com>
2020-05-06 15:32:13 -0400

    first commit

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
     public static void main(String[] args) {
-        Product product = new Product("code", "description", 120);
-        Product productCopy = new Product("code", "description", 120);
+
+
         Book book = new Book("code book", "description book", 42, "Some author", 300);
         Book bookCopy = new Book("code book", "description book", 42, "Some author", 300);
         Software software = new Software("code software", "description software", 80,
@@ -9,10 +9,7 @@ public class Driver {
         Software softwareCopy = new Software("code software", "description software", 80,
                 "Some programmer", "Some platform", "Some OS");
 
-        System.out.println(product.equals(productCopy));
         System.out.println(book.equals(bookCopy));
         System.out.println(software.equals(softwareCopy));
-        System.out.println(book.equals(product));
-        System.out.println(product.equals(software));
     }
 }
